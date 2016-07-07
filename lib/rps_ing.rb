e8071e0bb919e0e7c14307f69d5edcf7a12b0802
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-07 09:43:27 -0700

    added player, game, and computer methods

--- lib/rps_ing.rb
+++ lib/rps_ing.rb
@@ -1,26 +1,45 @@
 require "rps_ing/version"
 
 module RpsIng
+  include Display
   class Game
     def initialize
       @player = Player.new
+      @pick_arr = %w[r p s]
     end
 
     def play
       intro
       loop do
-        player_input
-        break if game_over?
+        @player.ask_input
+        break if victory?
       end
       results
     end
 
-    def game_over
+    def victory?
+      comp_pick = Computer.new.get_input
+      win?(comp_pick) || lose?(comp_pick) || tie?(comp_pick)
+
+      @player.player_input.find_index(@pick_arr)
+
     end
 
-    def player_input
+    def index_finder(player)
+      @pick_arr.find_index(player.get_input)
     end
 
+    def win?
+      find_index
+    end
+
+    def lose?
+
+    end
+
+    def tie?
+
+    end
   end
 
   class Player
@@ -32,20 +51,39 @@ module RpsIng
     end
 
     def ask_input
+      choice_message
+      get_input
+    end
+
+    def valid_input?(input)
+      %[r p s].include?(input)
     end
 
   end
 
   class Human < Player
+    attr_reader :player_input
+
+    def get_input
+        @player_input = gets.strip until valid_input?(@player_input)
+    end
+
   end
 
   class Computer < Player
+
+    def get_input
+      %w[r p s].sample
+    end
   end
 
   module Display
 
     def intro
       puts "Welcome to Rock, Paper, Scissors"
+    end
+
+    def choice_message
       puts "Please make your choice"
     end
 
