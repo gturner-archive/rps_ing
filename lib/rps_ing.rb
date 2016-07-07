9c1171e6e11ba2f8ca8f51682ff55ee09f8f88e7
Graham Turner <turnertgraham@gmail.com>
2016-07-07 12:18:01 -0400

    added classes from psudeocode

--- lib/rps_ing.rb
+++ lib/rps_ing.rb
@@ -1,5 +1,57 @@
 require "rps_ing/version"
 
 module RpsIng
-  # Your code goes here...
+  class Game
+    def initialize
+      @player = Player.new
+    end
+
+    def play
+      intro
+      loop do
+        player_input
+        break if game_over?
+      end
+      results
+    end
+
+    def game_over
+    end
+
+    def player_input
+    end
+
+  end
+
+  class Player
+
+    def validate_input
+    end
+
+    def compare_input
+    end
+
+    def ask_input
+    end
+
+  end
+
+  class Human < Player
+  end
+
+  class Computer < Player
+  end
+
+  module Display
+
+    def intro
+      puts "Welcome to Rock, Paper, Scissors"
+      puts "Please make your choice"
+    end
+
+    def results
+    end
+
+  end
+
 end
